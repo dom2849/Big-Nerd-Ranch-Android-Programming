1d6f912bd741483ece6342ea1124dae6faae3cbf
Domenico Manna <mannadomenico2849@gmail.com>
2018-10-21 19:50:28 -0400

    Track the number of questions answered correctly

--- Quiz/app/src/main/java/com/example/domenicomanna/quiz/MainActivity.java
+++ Quiz/app/src/main/java/com/example/domenicomanna/quiz/MainActivity.java
@@ -12,13 +12,19 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private static final String KEY_INDEX = "index";
+    private static final String NUMBER_OF_ANSWERS_RIGHT = "number of answers right";
+    private static final String DISABLE_TRUE_AND_FALSE_BUTTONS = "disable true and false buttons";
+    private static final String RIGHT_ANSWER_FEEDBACK = "Good job!";
+    private static final String WRONG_ANSWER_FEEDBACK = "That is wrong.";
+    private static final String RETAKE_QUIZ = "Retake the quiz to score higher!";
+    private static final String NICE_SCORE = "You scored well! Nice!";
 
     private Button btn_True;
     private Button btn_False;
     private ImageButton btn_next;
-    private ImageButton btn_previous;
     private TextView text_question;
     private int currentIndex = 0;
+    private int numberOfCorrectAnswers = 0;
     private Question [] questions = new Question [] {
         (new Question(R.string.question_2add2, true)),
         (new Question(R.string.question_2into2, true)),
@@ -28,20 +34,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate: Called");
-
-        if (savedInstanceState != null){
-            currentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
-        }
-
         setContentView(R.layout.activity_main);
-
         btn_True = findViewById(R.id.btn_true);
         btn_False = findViewById(R.id.btn_false);
         btn_next = findViewById(R.id.btn_next);
-        btn_previous = findViewById(R.id.btn_previous);
         text_question = findViewById(R.id.question_display);
+        restorePreviousData(savedInstanceState);
         changeQuestion(currentIndex);
         configureClickListeners();
+    }
+
+    /**
+     *
+     * @param savedInstanceState holds the data that will be restored
+     */
+    private void restorePreviousData(Bundle savedInstanceState) {
+        if (savedInstanceState != null){
+            currentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+            numberOfCorrectAnswers = savedInstanceState.getInt(NUMBER_OF_ANSWERS_RIGHT, 0);
+            boolean trueAndFalseButtonsShouldBeDisabled = savedInstanceState.getBoolean(DISABLE_TRUE_AND_FALSE_BUTTONS, false);
+            if (trueAndFalseButtonsShouldBeDisabled){
+                disableTrueAndFalseButtons();
+            }
+
+        }
     }
 
     /**
@@ -51,7 +67,6 @@ public class MainActivity extends AppCompatActivity {
         btn_True.setOnClickListener((View v) -> checkIfAnswerIsTrue());
         btn_False.setOnClickListener((View v) -> checkIfAnswerIsFalse());
         btn_next.setOnClickListener((View v) -> goToNextQuestion());
-        btn_previous.setOnClickListener((View v) -> goToPreviousQuestion());
     }
 
     /**
@@ -61,12 +76,13 @@ public class MainActivity extends AppCompatActivity {
         Question current = questions[currentIndex];
         boolean questionIsTrue = current.getAnswer();
         if (questionIsTrue){
-            makeToast("Good job!");
+            numberOfCorrectAnswers++;
+            makeToast(RIGHT_ANSWER_FEEDBACK);
             disableTrueAndFalseButtons();
         }
         else {
-            makeToast("Try again!");
-            btn_True.setEnabled(false);
+            makeToast(WRONG_ANSWER_FEEDBACK);
+            disableTrueAndFalseButtons();
         }
     }
 
@@ -77,12 +93,13 @@ public class MainActivity extends AppCompatActivity {
         Question current = questions[currentIndex];;
         boolean answer = current.getAnswer();
         if (!answer){
-            makeToast("Good job!");
+            numberOfCorrectAnswers++;
+            makeToast(RIGHT_ANSWER_FEEDBACK);
             disableTrueAndFalseButtons();
         }
         else {
-            makeToast("Try again!");
-            btn_False.setEnabled(false);
+            makeToast(WRONG_ANSWER_FEEDBACK);
+            disableTrueAndFalseButtons();
         }
     }
 
@@ -110,18 +127,8 @@ public class MainActivity extends AppCompatActivity {
         currentIndex++;
         if (currentIndex == questions.length){
             currentIndex = 0;
-        }
-        changeQuestion(currentIndex);
-    }
-
-    /**
-     * Goes to the previous question
-     */
-    private void goToPreviousQuestion(){
-        reactivateTrueAndFalseButtons();
-        currentIndex--;
-        if (currentIndex == -1){
-            currentIndex = questions.length-1;
+            showScoreReport();
+            numberOfCorrectAnswers = 0;
         }
         changeQuestion(currentIndex);
     }
@@ -133,6 +140,24 @@ public class MainActivity extends AppCompatActivity {
     private void changeQuestion(int currentIndex) {
         Question question = questions[currentIndex];
         text_question.setText(question.getResStringQuestion());
+    }
+
+    /**
+     * A toast message showing the number of correct answers out of the total number
+     * of questions
+     */
+    private void showScoreReport() {
+        int numberOfQuestionsRightAdjusted = numberOfCorrectAnswers;
+        if (numberOfCorrectAnswers >questions.length){
+            numberOfQuestionsRightAdjusted = questions.length;
+        }
+        String toDisplay = String.format("%d/%d right!", numberOfQuestionsRightAdjusted, questions.length);
+        if (numberOfCorrectAnswers < 2){
+            makeToast(toDisplay + " " + RETAKE_QUIZ);
+        }
+        else {
+            makeToast(toDisplay + " " + NICE_SCORE);
+        }
     }
 
     /**
@@ -186,5 +211,12 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(savedInstanceState);
         Log.d(TAG, "onSaveInstanceState: Called");
         savedInstanceState.putInt(KEY_INDEX, currentIndex);
+        savedInstanceState.putInt(NUMBER_OF_ANSWERS_RIGHT, numberOfCorrectAnswers);
+        if (btn_True.isEnabled()) {
+            savedInstanceState.putBoolean(DISABLE_TRUE_AND_FALSE_BUTTONS, false);
+        }
+        else {
+            savedInstanceState.putBoolean(DISABLE_TRUE_AND_FALSE_BUTTONS, true);
+        }
     }
 }
